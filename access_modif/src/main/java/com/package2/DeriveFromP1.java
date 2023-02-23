07e2bc233764880ae25ece5272e2e57ae4479007
micserv010821 <dotnet290320@gmail.com>
2023-02-23 19:30:47 +0200

    upload

--- access_modif/src/main/java/com/package2/DeriveFromP1.java
+++ access_modif/src/main/java/com/package2/DeriveFromP1.java
@@ -4,10 +4,23 @@ import com.package1.BaseP1;
 
 public class DeriveFromP1 extends BaseP1 {
 
+    public DeriveFromP1() {
+        super();
+    }
+
     public void foo3() {
         // default -- subclass different package -- CANNOT access
         //System.out.println(this.i_am_default);
 
         System.out.println(this.i_am_protected);
+
+        this.func1();
+        func1();
+        super.func1();
+    }
+
+    @Override
+    public void func1() {
+
     }
 }
